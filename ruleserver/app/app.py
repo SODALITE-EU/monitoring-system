ff61a36fd4931f8dcc603d6e06c19f3b877987f1
Jesús Ramos Rivas <jesus.ramos.external@atos.net>
2021-07-22 12:09:41 +0200

    update to include changes in monitoring stack

--- ruleserver/app/app.py
+++ ruleserver/app/app.py
@@ -16,10 +16,15 @@ def validate_file(input_path):
 		os.remove(input_path)
 		return False
 
-def validate_modelid(model_id):
-	return (model_id is not None)
+def validate_monitoring_id(monitoring_id, presence):
+	if monitoring_id is None:
+		return False
+	if not presence: 
+		return True
+	else:
+		return os.path.isfile("/etc/prometheus/rules/%s.yml" % monitoring_id)
 
-def register_rule_file(filepath,model_id):
+def register_rule_file(filepath):
 	try:
 		print(filepath)
 		shutil.copy(filepath,'/etc/prometheus/rules')
@@ -28,9 +33,9 @@ def register_rule_file(filepath,model_id):
 	except OSError:
 		return False
 
-def unregister_rule_file(model_id):
+def unregister_rule_file(monitoring_id):
 	try:
-		os.remove("/etc/prometheus/rules/rules_%s.yml" % model_id)
+		os.remove("/etc/prometheus/rules/%s.yml" % monitoring_id)
 		return True
 	except FileNotFoundError:
 		return False
@@ -42,12 +47,12 @@ def reload_prometheus_configuration():
 	check = str(out).find("HTTP/1.0 204 No Content")
 	return (check > 0)
 
-@app.route('/rules/add/<model_id>', methods=['POST'])
-def add_rule_file(model_id=None):
-	if not validate_modelid(model_id):
+@app.route('/rules/<monitoring_id>', methods=['POST'])
+def add_rule_file(monitoring_id=None):
+	if not validate_monitoring_id(monitoring_id, False):
 		return "Model id is not valid\n", 400
 
-	input_path = "/tmp/rules_%s.yml" % model_id
+	input_path = "/tmp/%s.yml" % monitoring_id
 	f = open(input_path, "bw")
 	f.write(request.get_data())
 	f.close()
@@ -55,24 +60,24 @@ def add_rule_file(model_id=None):
 	if not validate_file(input_path):
 		return "Rule file did not pass the promtool check\n", 400
 	else:
-		if register_rule_file(input_path,model_id):
+		if register_rule_file(input_path):
 			if reload_prometheus_configuration():
-				return ("Updated Prometheus config with a rule file for model id %s\n" % model_id), 200
+				return ("Updated Prometheus config with a rule file for monitoring id %s\n" % monitoring_id), 200
 			else:
-				return "Rule file was added but config reload failed\n", 400
+				return "Rule file was added but config reload failed\n", 500
 		else:
-			return "Rule file could not be copied to Prometheus rules folder\n", 400
+			return "Rule file could not be copied to Prometheus rules folder\n", 500
 
-@app.route('/rules/remove/<model_id>', methods=['GET'])
-def remove_rule_file(model_id=None):
+@app.route('/rules/<monitoring_id>', methods=['DELETE'])
+def remove_rule_file(monitoring_id=None):
 
-	if not validate_modelid(model_id):
-		return "Model id is not valid\n", 400
+	if not validate_monitoring_id(monitoring_id, True):
+		return ("Could not find ruleset for monitoring_id %s\n" % monitoring_id),  404
 
-	if unregister_rule_file(model_id):
+	if unregister_rule_file(monitoring_id):
 		if reload_prometheus_configuration():
-			return ("Updated Prometheus config with rules for model id %s removed\n" % model_id), 200
+			return ("Updated Prometheus config with rules for monitoring id %s removed\n" % monitoring_id), 200
 		else:
-			return "Rule file was removed but config reload failed\n", 400
+			return "Rule file was removed but config reload failed\n", 500
 	else:
-		return ("Rule file for model %s has been not found\n" % model_id), 400
+		return ("Rule file for monitoring id %s could not be deleted\n" % monitoring_id), 500
